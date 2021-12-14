d0da03e8af3a43b91c0c462ae46e1d2d7730c479
ehuayllasco <64268570+ehuayllasco@users.noreply.github.com>
2021-12-14 09:47:30 -0500

    refactoring Ed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require_once("config/conexion.php");
-    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
+    if(isset($_POST["enviar"]) && $_POST["enviar"]=="si"){
         require_once("models/Usuario.php");
         $usuario = new Usuario();
         $usuario->login();
