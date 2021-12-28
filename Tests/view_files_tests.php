512b0fe6d4530776050d9b3c0e414e19c9e6f9f0
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2021-12-28 08:03:15 -0500

    COmposer

---
+++ Tests/view_files_tests.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    private $test_var;
+
+    public function setUp():void
+    {
+        $this->test_var= new Email();
+    }
+
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
